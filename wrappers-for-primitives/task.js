56e5e72706daa1c5773499b98258ff6c9d8c3cd1
Landysh Khusaenova <landysh.kg@gmail.com>
2019-03-11 20:48:20 +0300

    Задание 1 выполнено

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,3 +1,5 @@
+'use strict'
+
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -10,10 +12,32 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+  // количество месяцев 
+  let dateOfEnd = new Date(date).getFullYear();
+  let n = (dateOfEnd - new Date().getFullYear()) * 12;
+  console.log(n);
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
-}
+  //сумма, которую необходимо вернуть банку
+  let refundSum = parseFloat(amount) - parseFloat(contribution);
+    console.log(refundSum);
+
+  //процентная ставка
+  let interestRate = parseFloat(percent) / 100;
+  console.log(interestRate);
+  
+  //Ежемесячная оплата 
+  let p = interestRate * (1 / 12);
+  let monthlyFee = refundSum * (p + p / (Math.pow((1 + p), n) - 1));
+  console.log(monthlyFee);
+
+  //сумма процентов за пользование кредитом
+  let creditPercent = (monthlyFee * n) - refundSum;
+  console.log(creditPercent);
+
+  //общая сумма, которую придется заплатить клиенту
+  let totalAmount = parseFloat(contribution) + refundSum + creditPercent;
+  return totalAmount.toFixed(2);
+};
 
 function sayHello() {
     let name = window.personName.value;
