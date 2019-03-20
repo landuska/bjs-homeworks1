9fcd1e904a23b0dcf11ed99a8e766dd9b9626178
Landysh Khusaenova <landysh.kg@gmail.com>
2019-03-20 23:55:10 +0300

    Задание по теме «Прототип массива, функции высшего порядка»

--- array-proto-hocs/main.js
+++ array-proto-hocs/main.js
@@ -1,28 +1,47 @@
 function compareArrays(arr1, arr2){
-    //создадим переменную, которая будет хранить булевые значения проверки двух массивов
-    const repository = [];
-
-    // проверяем все эдементы массивов на соответсвие значений и индексов
-    for (let i = 0; i < arr1.length; i ++){
-        (arr2.includes(arr1[i]) && ((arr1.indexOf(arr1[i])) === (arr2.indexOf(arr1[i])))) ? (repository.push(true)) : (repository.push(false));
-    };
-    //проверяем, был ли хоть один false  в сравнении массивов
-    return (repository.includes(false)) ? false : true;
-};    
-
-function memoize(compareArrays, limit){
-    //создадим массив, куда будем записывать значения вычислений и параметр, который будет хранить историю вызовов возвращаемой memoize функции
-    let results = {
-        args: [arr1, arr2], // массив аргументов, с которыми была вызывана функция
-        result: compareArrays(arr1, arr2) // результат работы
-    };
-    
-    if (results.args !== undefined){
-        return results.result;
-    } else {
-        let boolingValue = compareArrays(arr1, arr2);
-        //Добавляем результат вычислений и вырезаем массив от 0 элемента до 10
-        results[result].push(boolingValue).slice(0, limit);
-        return compareArrays(arr1, arr2);
+    //сравниваем массивы
+    if (arr1.length === arr2.length){
+        return arr1.every((item, index) => item === arr2[index]);
+    }else{
+        return false;
     };    
+};       
+
+function memoize(sum, limit){
+    let results = {
+        args : [], // аргументы вызова возвращаемой функции fn
+        result : []// результат работы
+    };
+
+    let arrayOfResults = [];
+
+    function fn(a, b){
+        if (arrayOfResults.length === 0){
+            results.args =  [a, b];
+            results.result = sum(a, b);
+            arrayOfResults.push(results);
+            return console.log(results.result);
+        }else{    
+            if (compareArrays(results.args, [a, b])){
+                return console.log('Результат берется из памяти \n' + results.result);
+            }else{ 
+                results.args =  [a, b];
+                results.result = sum(a, b);
+                arrayOfResults.push(results);
+                return console.log('Функция вызвана не из памяти\n' + results.result); 
+            };
+        };    
+    };
+
+    return fn;
 };
+
+const sumfn = (a, b) => a + b;
+
+sumfn (3, 4)
+const mSum = memoize(sumfn, 10)
+
+mSum(3, 4)
+mSum(4, 5)
+mSum(3, 4)
+mSum(2, 4)
